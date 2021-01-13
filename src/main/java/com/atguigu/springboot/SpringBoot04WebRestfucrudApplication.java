411e8d99a68c1a92a271afbd5521ef8d4e9dec04
jiuduweiliangeer <64030931@qq.com>
2021-01-13 16:23:54 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/springboot/SpringBoot04WebRestfucrudApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04WebRestfucrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot04WebRestfucrudApplication.class, args);
+    }
+
+}
